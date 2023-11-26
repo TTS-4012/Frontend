8b932c2b2dd117b9571609d9820363d0717469b1
Mobin Azadani <azad.1214mobin@gmail.com>
2023-11-26 22:55:47 +0330

    fix: Home page accessble only for authorized user

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,8 +1,7 @@
-// import { Navigate, Outlet } from "react-router-dom";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 function Main() {
-  // if (!localStorage.getItem("auth.access_token")) return <Navigate to="/login" />;
+  if (!localStorage.getItem("auth.access_token")) return <Navigate to="/login" />;
   return (
     <div className="flex h-screen w-screen bg-indigo-100">
       <div className="m-auto w-full max-w-md rounded-lg bg-white p-3 shadow">
