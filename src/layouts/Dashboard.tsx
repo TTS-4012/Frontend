98d8e2c476188f3b2bd6e5ff16c22f917d09f2ca
Mobin Azadani <azad.1214mobin@gmail.com>
2023-11-28 21:47:55 +0330

    fix: auth added back

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import Header from "./Header";
 
 function Dashboard() {
-  // if (!localStorage.getItem("auth.access_token")) return <Navigate to="/login" />;
+  if (!localStorage.getItem("auth.access_token")) return <Navigate to="/login" />;
   return (
     <div className="flex h-screen w-screen flex-col bg-indigo-100">
       <Header />
