75d850262c0d043fefec5b288e533ebfde8fe15e
Mobin Azadani <azad.1214mobin@gmail.com>
2023-12-21 21:19:26 +0330

    feat: basic clock

---
+++ src/pages/Problems/ContestProblem.tsx
@@ -0,0 +1,139 @@
+import ProblemComponent from "../../components/ProblemComponent";
+import axios, { AxiosError } from "axios";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import LinkLabel from "../../components/LinkLabel";
+import { duration } from "@mui/material";
+
+type ProblemDataType = {
+  problem_id: number;
+  title: string;
+};
+type ContestDataType = {
+  contest_Id: number;
+  title: string;
+  porblems: ProblemDataType[];
+  start_time: Date;
+  duration: number;
+};
+
+type ParamsType = {
+  contestId: string;
+  problemId: string;
+};
+
+function ContestProblem() {
+  const { contestId, problemId } = useParams<ParamsType>();
+  const contest_bar = ["Ranking", "Uplaod History"];
+  const [contestData, setContestmData] = useState<ContestDataType>({
+    contest_Id: 10,
+    title: "Contest Name",
+    porblems: [
+      { problem_id: 1, title: "problem1" },
+      { problem_id: 2, title: "problem2" },
+      { problem_id: 3, title: "problem3" },
+    ],
+    start_time: new Date("2023-12-21T22:00:00"),
+    duration: 6000,
+  });
+  const [reverseTimer, setTimer] = useState<number>(0);
+  const [errorMessage, setErrorMessage] = useState<string>();
+
+  useEffect(() => {
+    setErrorMessage("");
+    setInterval(
+      () => setTimer(contestData.start_time.valueOf() - new Date().valueOf() + contestData.duration * 60 * 1000),
+      1000,
+    );
+    axios
+      .get<ContestDataType>("/contest/" + String(contestId), {
+        headers: { Authorization: localStorage.getItem("auth.access_token") },
+      })
+      .then((res) => {
+        setContestmData(res.data);
+      })
+      .catch((err: AxiosError<any>) => {
+        console.log(err.message);
+        setErrorMessage(err.response?.data.message ?? err.message);
+      });
+  }, [contestId, problemId]);
+
+  return (
+    problemId && (
+      <>
+        {/* add !*/}
+        {errorMessage && (
+          <div className="flex flex-row">
+            <div className=" basis-5/6">
+              <ProblemComponent id={problemId}></ProblemComponent>;
+            </div>
+
+            <div className="mb-auto mr-5 mt-5 flex basis-1/6 flex-col rounded-lg pb-5 text-center">
+              <div className="flex flex-col rounded-lg bg-gray-100 shadow-md">
+                <p className="rounded-t-lg bg-gray-300 py-1.5 text-xl font-medium"> {contestData?.title} </p>
+
+
+                <p>
+                  {contestData.start_time.toLocaleDateString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </p>
+                <p>
+                  {contestData.start_time.toLocaleString("en-US", {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  })}
+                </p>
+                <p>
+                  {reverseTimer / 60000},{reverseTimer % 60000}
+                </p>
+
+                
+                {contestData?.porblems.map((problem, i) => (
+                  <label
+                    key={i}
+                    className="rounded-md hover:bg-white">
+                    {problemId != String(problem.problem_id) ? (
+                      <LinkLabel
+                        to={"/contests/" + String(contestId) + "/" + String(problem.problem_id)}
+                        className="inline w-10 px-2 text-lg">
+                        {problem.title.slice(0, 15)} {problem.title.length > 15 && "..."}
+                      </LinkLabel>
+                    ) : (
+                      <LinkLabel
+                        to={"/contests/" + String(contestId) + "/" + String(problem.problem_id)}
+                        className=" inline w-10 px-2 text-lg font-semibold text-indigo-400">
+                        {problem.title.slice(0, 15)} {problem.title.length > 15 && "..."}
+                      </LinkLabel>
+                    )}
+                  </label>
+                ))}
+              </div>
+
+              <span className="py-2"> </span>
+              <div className="flex flex-col rounded-lg bg-gray-100 shadow-md ">
+                {contest_bar.map((title) => (
+                  <label
+                    // key={j}
+                    className="rounded-md py-1 hover:bg-white">
+                    <LinkLabel
+                      to={"/" + title}
+                      className="inline w-10 px-2 text-xl">
+                      {title}
+                    </LinkLabel>
+                  </label>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {/* {errorMessage && <p className="m-auto text-center text-5xl text-indigo-800">{errorMessage}</p>} */}
+      </>
+    )
+  );
+}
+
+export default ContestProblem;
