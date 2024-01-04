646af8f7467e1dfd1d4e44ccc2babc4705add9b3
MoJavad Jalilvand <jalilvand.mj@gmail.com>
2024-01-04 23:06:02 +0330

    feat: contest settings button

--- src/pages/Contest/ContestProblem.tsx
+++ src/pages/Contest/ContestProblem.tsx
@@ -10,6 +10,7 @@ import {
   TrophyIcon,
   PencilIcon,
   PlusCircleIcon,
+  Cog6ToothIcon,
 } from "@heroicons/react/24/outline";
 import Button from "../../components/Button";
 
@@ -45,55 +46,49 @@ function ContestDuration({ contestData }: { contestData: ContestDataType }) {
   }, [contestData]);
 
   return (
-    <div className="flex flex-col gap-1 rounded-t-lg bg-white p-1 shadow-md">
-      <p className="border-b border-gray-200 p-4 text-center text-3xl font-bold text-gray-900 sm:px-6">
-        {contestData.title}
-      </p>
-      <div className="flex flex-row gap-3 p-2">
-        {contestData.start_time - Math.floor(new Date().valueOf() / 1000) > 0 ? (
-          <>
-            <CalendarIcon className="m-auto h-6 w-6 shrink-0" />
-            <p className="flex grow flex-col">
-              <p className="text-lg font-medium">
-                {new Date(contestData.start_time * 1000).toLocaleDateString("en-GB", {
-                  day: "numeric",
-                  month: "short",
-                  year: "numeric",
-                })}
-              </p>
-              <p className="text-lg">
-                {new Date(contestData.start_time * 1000).toLocaleString("en-US", {
-                  hour: "numeric",
-                  minute: "numeric",
-                  hour12: true,
-                })}
-              </p>
+    <div className="flex flex-row gap-3 p-2">
+      {contestData.start_time - Math.floor(new Date().valueOf() / 1000) > 0 ? (
+        <>
+          <CalendarIcon className="m-auto h-6 w-6 shrink-0" />
+          <p className="flex grow flex-col">
+            <p className="text-lg font-medium">
+              {new Date(contestData.start_time * 1000).toLocaleDateString("en-GB", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+              })}
             </p>
-          </>
-        ) : contestData.start_time + contestData.duration - Math.floor(new Date().valueOf() / 1000) > 0 ? (
-          <>
-            <div className="mx-auto flex flex-row gap-2 py-2">
-              <ClockIcon className="m-auto h-6 w-6 shrink-0" />
-              <p
-                className={`grow text-lg font-medium flex${
-                  (reverseTimer < 600 && reverseTimer >= 600 && "text-yellow-600") ||
-                  (reverseTimer < 60 && "text-red-600")
-                }`}>
-                {Math.floor(reverseTimer / 3600).toString().length == 1 && "0"}
-                {Math.floor(reverseTimer / 3600)}:{" "}
-                {Math.floor((reverseTimer % 3600) / 60).toString().length == 1 && "0"}
-                {Math.floor((reverseTimer % 3600) / 60)}: {Math.floor(reverseTimer % 60).toString().length == 1 && "0"}
-                {Math.floor(reverseTimer % 60)}
-              </p>
-            </div>
-          </>
-        ) : (
-          <>
-            <CalendarIcon className="m-auto h-6 w-6 shrink-0" />
-            <p className="grow px-2 text-lg italic">This contest is finished</p>
-          </>
-        )}
-      </div>
+            <p className="text-lg">
+              {new Date(contestData.start_time * 1000).toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })}
+            </p>
+          </p>
+        </>
+      ) : contestData.start_time + contestData.duration - Math.floor(new Date().valueOf() / 1000) > 0 ? (
+        <>
+          <div className="mx-auto flex flex-row gap-2 py-2">
+            <ClockIcon className="m-auto h-6 w-6 shrink-0" />
+            <p
+              className={`grow text-lg font-medium flex${
+                (reverseTimer < 600 && reverseTimer >= 600 && "text-yellow-600") ||
+                (reverseTimer < 60 && "text-red-600")
+              }`}>
+              {Math.floor(reverseTimer / 3600).toString().length == 1 && "0"}
+              {Math.floor(reverseTimer / 3600)}: {Math.floor((reverseTimer % 3600) / 60).toString().length == 1 && "0"}
+              {Math.floor((reverseTimer % 3600) / 60)}: {Math.floor(reverseTimer % 60).toString().length == 1 && "0"}
+              {Math.floor(reverseTimer % 60)}
+            </p>
+          </div>
+        </>
+      ) : (
+        <>
+          <CalendarIcon className="m-auto h-6 w-6 shrink-0" />
+          <p className="grow px-2 text-lg italic">This contest is finished</p>
+        </>
+      )}
     </div>
   );
 }
@@ -139,45 +134,36 @@ function ContestProblem() {
               {problemId == "0" && "No problems to show"}
             </p>
           )}
-          <div className="flex basis-1/6 flex-col gap-2">
-            <ContestDuration contestData={contestData} />
+          <div className="flex min-w-[16rem] basis-1/6 flex-col gap-2">
+            <div className="flex flex-col gap-1 rounded-t-lg bg-white p-1 shadow-md">
+              <div className="relative border-b border-gray-200 p-4 text-center text-3xl font-bold text-gray-900 sm:px-6">
+                {contestData.title}
+                <Button
+                  className="absolute inset-y-0 right-4 my-auto"
+                  size="zero"
+                  variant="inline"
+                  onClick={() => {
+                    navigate(`/contests/${contestId}/edit`);
+                  }}>
+                  <Cog6ToothIcon className="h-5 w-5" />
+                </Button>
+              </div>
+              <ContestDuration contestData={contestData} />
+            </div>
             {contestData.problems?.length > 0 && (
               <div className="flex flex-col space-y-1 bg-white p-1 shadow-md">
-                <div className="relative border-b border-gray-200 p-2 font-medium text-gray-900 sm:px-4">
-                  Problems
-                  <div className="absolute inset-y-0 right-4 my-auto flex gap-2">
-                    <Button
-                      size="zero"
-                      variant="inline"
-                      onClick={() => {
-                        navigate(`/contests/${contestId}/new`);
-                      }}>
-                      <PlusCircleIcon className="h-5 w-5" />
-                    </Button>
-                  </div>
-                </div>
+                <div className="border-b border-gray-200 p-2 font-medium text-gray-900 sm:px-4">Problems</div>
                 {contestData.problems?.map((problem, i) => (
-                  <div
+                  <Link
                     key={i}
+                    to={`/contests/${contestId}/problems/${problem.ID}`}
                     className={`flex items-center rounded-md px-3 py-2 text-sm font-medium ${
                       problemId == String(problem.ID)
                         ? "bg-gray-100 text-gray-900"
                         : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
                     }`}>
-                    <Link
-                      to={`/contests/${contestId}/problems/${problem.ID}`}
-                      className="mr-auto">
-                      {problem.Title.slice(0, 15)} {problem.Title.length > 15 && "..."}
-                    </Link>
-                    <Button
-                      size="zero"
-                      variant="inline"
-                      onClick={() => {
-                        navigate(`${problem.ID}/edit`);
-                      }}>
-                      <PencilIcon className="-m-1 h-5 w-5" />
-                    </Button>
-                  </div>
+                    {problem.Title.slice(0, 15)} {problem.Title.length > 15 && "..."}
+                  </Link>
                 ))}
               </div>
             )}
