5f970df51dca8431c7b8d0907da858d3fd0d0b2e
Mobin Azadani <azad.1214mobin@gmail.com>
2024-01-05 14:08:54 +0330

    chore: prettier

--- src/pages/Contest/ContestList.tsx
+++ src/pages/Contest/ContestList.tsx
@@ -90,11 +90,7 @@ function ListContests() {
   }, [page, rowsPerPage, started, decsendingTable]);
 
   const handleClick = (_: unknown, contest_id: number) => {
-<<<<<<< HEAD
     navigate(`/contests/${String(contest_id)}/0`);
-=======
-    navigate("/contests/" + String(contest_id) + "/0");
->>>>>>> 67dd149 (fix: bug path fixed)
   };
   const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
