811aede6a3f0d77042617d77e57a2914cfebb57a
MoJavad Jalilvand <jalilvand.mj@gmail.com>
2024-01-05 19:25:22 +0330

    feat: copy problem to contest

--- src/components/CopyToContest.tsx
+++ src/components/CopyToContest.tsx
@@ -1,13 +1,14 @@
 import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Button from "./Button";
 
 type ContestDataType = {
   contest_Id: number;
   title: string;
 };
 
-function CopyToContest() {
+function CopyToContest(props: { onClose: () => void }) {
   const { problemId } = useParams();
 
   const [contests, setContests] = useState<ContestDataType[]>();
@@ -30,24 +31,35 @@ function CopyToContest() {
   }, []);
 
   const handleAdd = (contestId: number) => {
-    console.log(contestId, problemId);
+    axios
+      .post(`contests/${contestId}/problems/${problemId}`)
+      .then(() => {
+        props.onClose();
+      })
+      .catch((err: AxiosError<any>) => {
+        console.log(err.message);
+        setErrorMessage(err.response?.data.message ?? err.message);
+      });
   };
 
   return (
     <div>
       Choose a contest from list below:
-      <ul
+      <div
         role="list"
-        className="divide-y divide-gray-200">
+        className="flex flex-col divide-y divide-gray-200">
         {contests?.map((contest) => (
-          <li
+          <Button
+            size="lg"
+            variant="inline"
             key={contest.contest_Id}
-            onClick={() => handleAdd(contest.contest_Id)}>
-            <p className="py-4 text-sm font-medium text-gray-900">{contest.title}</p>
-          </li>
+            onClick={() => handleAdd(contest.contest_Id)}
+            className="font-medium text-gray-900">
+            {contest.title}
+          </Button>
         ))}
-        <p>{errorMessage}</p>
-      </ul>
+      </div>
+      <p>{errorMessage}</p>
     </div>
   );
 }
