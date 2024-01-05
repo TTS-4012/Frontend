b16ab465a2c8519645a2aa3d7c522ff2f86f1276
Reza_Yazdi <yazdireza2003@gmail.com>
2024-01-05 17:39:00 +0330

    chore: navigate

--- src/pages/Contest/ListContests.tsx
+++ src/pages/Contest/ListContests.tsx
@@ -15,7 +15,6 @@ import Paper from "@mui/material/Paper";
 import { useNavigate } from "react-router-dom";
 import axios, { AxiosError } from "axios";
 import { useState, useEffect } from "react";
-import Dialog from "../../components/Dialog";
 
 function TablePaginationActions(props: {
   count: number;
@@ -44,7 +43,7 @@ function TablePaginationActions(props: {
       </IconButton>
       <IconButton
         onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        disabled={count != -1 && page >= Math.ceil(count / rowsPerPage) - 1}
         aria-label="next page">
         {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
       </IconButton>
@@ -53,49 +52,31 @@ function TablePaginationActions(props: {
 }
 
 type ContestDataType = {
-  total_count: number;
-  contests: {
-    contest_Id: number;
-    title: string;
-    register_status: number;
-  }[];
-};
-
-type FilterDataType = {
-  started: boolean | undefined;
-  my_contest: boolean | undefined;
-  owned_contest: boolean | undefined;
+  contest_Id: number;
+  title: string;
 };
 
 function ListContests() {
   const decsendingTable = false;
-  const [filterData, setFilterData] = useState<FilterDataType>({
-    started: false,
-    my_contest: false,
-    owned_contest: false,
-  });
+  const [started, setStarted] = useState<boolean>(false);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(20);
-  const [openDialog, setOpenDialog] = useState(false);
-  const [openDialogJoinFirst, setOpenDialogJoinFirst] = useState(false);
 
-  const [tableData, setTableData] = useState<ContestDataType>();
+  const [tableData, setTableData] = useState<ContestDataType[]>();
+  const [errorMessage, setErrorMessage] = useState<string>();
 
-  const [toggleUpdateData, UpdateTableData] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
-  const [errorMessageJoinContest, setErrorMessageJoinContest] = useState<string>("");
   const navigate = useNavigate();
 
   useEffect(() => {
     setErrorMessage("");
     axios
       .get<ContestDataType[]>("/contests", {
+        headers: { Authorization: localStorage.getItem("auth.access_token") },
         params: {
           descendig: decsendingTable,
           limit: rowsPerPage,
           offset: page * rowsPerPage,
-          started: filterData.started,
-          my_contest: filterData.my_contest,
+          started: started,
         },
       })
       .then((res) => {
@@ -105,11 +86,10 @@ function ListContests() {
         console.log(err.message);
         setErrorMessage(err.response?.data.message ?? err.message);
       });
-    console.log(tableData);
-  }, [page, rowsPerPage, filterData, decsendingTable, toggleUpdateData]);
+  }, [page, rowsPerPage, started, decsendingTable]);
 
   const handleClick = (_: unknown, contest_id: number) => {
-    navigate(`/contests/${contest_id}/problems/0`);
+    navigate(`/contests/${String(contest_id)}/0`);
   };
   const handleChangePage = (_: unknown, newPage: number) => {
     setPage(newPage);
@@ -123,56 +103,17 @@ function ListContests() {
     <div className="mx-auto w-full max-w-7xl p-2">
       <div className="py-2">{errorMessage && <span className="ml-3 text-red-700">{errorMessage}</span>}</div>
       <div className="flex flex-col gap-2">
-        <Dialog
-          open={openDialog}
-          onClose={() => setOpenDialog(false)}
-          title="Join Contest">
-          <div>
-            <p>
-              {errorMessageJoinContest && "Could not join the contest"}
-              {!errorMessageJoinContest && "Succesfuly joined the contest"}
-            </p>
-          </div>
-        </Dialog>
-        <Dialog
-          open={openDialogJoinFirst}
-          onClose={() => setOpenDialogJoinFirst(false)}
-          title="Join Contest">
-          <p>Please first join the contest</p>
-        </Dialog>
         <div className="flex flex-row justify-start rounded-sm bg-slate-400 pl-3 shadow-sm">
-          <p className="px-5 py-2 font-bold">Filter by </p>
+          <p className="px-5 py-2 font-bold">Sort by </p>
           <button
-            className={`px-5 ${filterData.started && "bg-slate-300"}`}
-            onClick={() =>
-              setFilterData({
-                started: true,
-                my_contest: filterData.my_contest,
-                owned_contest: filterData.owned_contest,
-              })
-            }>
+            className={`px-5 ${started && "bg-slate-300"}`}
+            onClick={() => setStarted(true)}>
             Started
           </button>
           <button
-            className={`px-5 ${!filterData.started && "bg-slate-300"}`}
-            onClick={() =>
-              setFilterData({
-                started: false,
-                my_contest: filterData.my_contest,
-                owned_contest: filterData.owned_contest,
-              })
-            }>
-            Not Started yet
-          </button>
-          <button
-            className={`px-5 ${filterData.my_contest && "bg-slate-300"}`}
-            onClick={() => setFilterData({ started: filterData.started, my_contest: true, owned_contest: true })}>
-            My Contests
-          </button>
-          <button
-            className={`px-5 ${!filterData.my_contest && "bg-slate-300"}`}
-            onClick={() => setFilterData({ started: filterData.started, my_contest: false, owned_contest: false })}>
-            Other Contests
+            className={`px-5 ${!started && "bg-slate-300"}`}
+            onClick={() => setStarted(false)}>
+            Not Started
           </button>
         </div>
         <TableContainer component={Paper}>
@@ -184,59 +125,29 @@ function ListContests() {
                 <TableCell
                   align="left"
                   padding="checkbox">
-                  <p className="pl-2 font-bold">Number</p>
+                  <p className="pl-2 font-bold  ">Number</p>
                 </TableCell>
                 <TableCell align="center">
                   <p className="font-bold  ">Contest Title</p>
                 </TableCell>
-                <TableCell
-                  align="center"
-                  padding="checkbox">
-                  <p className="px-auto font-bold ">Joined</p>
-                </TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {tableData?.contests.length == 0 && (
-                <>
-                  <TableRow>
-                    <TableCell>#</TableCell>
-                    <TableCell align="center">
-                      <p className=" text-lg font-medium italic text-slate-500"> no problems</p>
-                    </TableCell>
-                    <TableCell>-</TableCell>
-                  </TableRow>
-                </>
-              )}
-              {tableData?.contests?.map((contest, i) => (
+              {tableData?.map((contest, i) => (
                 <TableRow
                   key={i}
-                  sx={{ cursor: "pointer" }}>
+                  onClick={(event) => handleClick(event, contest.contest_Id)}
+                  sx={{ cursor: "pointer" }}
+                  className="hover:bg-blue-100">
                   <TableCell
                     className=" bg-blue-50"
                     align="center">
                     {page * rowsPerPage + i + 1}
                   </TableCell>
-                  <TableCell
-                    align="center"
-                    onClick={(event) => handleClick(event, contest.contest_Id, contest.register_status)}
-                    className="rounded-sm hover:bg-blue-100">
+                  <TableCell align="center">
                     <p className="text-lg·font-medium">
                       {contest.title.slice(0, 25)} {contest.title.length > 25 && "..."}
                     </p>
-                  </TableCell>
-                  <TableCell align="center">
-                    {contest.register_status == 1 && <p className="py-1">EDIT</p>}
-                    {contest.register_status == 2 && <p className="py-1 italic">Joined</p>}
-                    {contest.register_status == 3 && (
-                      <>
-                        <button
-                          className=" rounded-md px-3 py-2 text-base hover:bg-slate-100"
-                          onClick={() => handleJoinContest(contest.contest_Id)}>
-                          Join
-                        </button>
-                      </>
-                    )}
                   </TableCell>
                 </TableRow>
               ))}
@@ -244,9 +155,10 @@ function ListContests() {
             <TableFooter>
               <TableRow>
                 <TablePagination
+                  className=""
                   rowsPerPageOptions={[20, 50, 100]}
                   colSpan={4}
-                  count={tableData?.total_count || -1}
+                  count={-1}
                   rowsPerPage={rowsPerPage}
                   page={page}
                   onPageChange={handleChangePage}
