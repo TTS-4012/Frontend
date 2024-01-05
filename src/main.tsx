4101b563a4eeeaecbf97794b3d519580f25b8029
Mobin Azadani <azad.1214mobin@gmail.com>
2024-01-05 21:35:48 +0330

    file rename2

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ import ListSubmissions from "./pages/Problems/Submissions/ListSubmissons.tsx";
 import Problem from "./pages/Problems/Problem.tsx";
 import Home from "./pages/Home.tsx";
 import axios from "axios";
-import ListContests from "./pages/Contest/ListContest.tsx";
+import ListContests from "./pages/Contest/ListContests.tsx";
 
 axios.defaults.baseURL = "https://api.ocontest.ir/v1";
 
