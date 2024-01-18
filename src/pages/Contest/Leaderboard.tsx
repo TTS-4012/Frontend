c6a8d787f31505d7eda4d4cffa446dc4b2f768de
Reza_Yazdi <yazdireza2003@gmail.com>
2024-01-19 00:03:19 +0330

    feat: scoreboard

--- src/pages/Contest/Leaderboard.tsx
+++ src/pages/Contest/Leaderboard.tsx
@@ -39,65 +39,65 @@ function Leaderboard() {
   }, [id, pageNumber, rowPerPage]);
 
   return (
-    <>
-      <div className="mx-auto w-full p-2">
-        {errorMessage ? (
-          <div>{errorMessage}</div>
-        ) : (
-          <table className="overflow-y-scroll">
-            <thead className="bg-gray-50">
-              <tr className=" rounded-t-md">
-                <td
+    <div className="w-screen p-2">
+      {errorMessage ? (
+        <div>{errorMessage}</div>
+      ) : (
+        <table className="mx-auto w-11/12 overflow-y-scroll rounded-md shadow-md">
+          <thead>
+            <tr className="bg-indigo-400">
+              <td
+                scope="col"
+                className="w-14 rounded-tl-md py-3.5 text-center text-sm font-semibold text-gray-900">
+                Rank
+              </td>
+              <td
+                scope="col"
+                className="w-1/12 py-3.5 text-center text-sm font-semibold text-gray-900">
+                User
+              </td>
+              {data?.problems.map((question) => (
+                <th
                   scope="col"
-                  className="py-3.5 text-sm font-semibold text-gray-900 ">
-                  Number
-                </td>
-                <td
-                  scope="col"
-                  className="py-3.5 text-sm font-semibold text-gray-900 ">
-                  User
-                </td>
-                {data?.problems.map((question) => (
-                  <th
-                    scope="col"
-                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                    <Link to={`/contests/${id}/${question.id}`}> {question.title}</Link>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody className="overflow-scroll">
-              {data?.users.map((user, idx) => (
-                <TableRow
-                  index={idx}
-                  user={user}
-                />
+                  className="py-3.5 pl-4 pr-3 text-center text-sm font-semibold text-gray-900 sm:pl-6">
+                  <Link to={`/contests/${id}/${question.id}`}> {question.title}</Link>
+                </th>
               ))}
-              <tr>
-                {data?.problems && (
-                  <td
-                    colSpan={data?.problems.length + 2}
-                    className="m-1 flex flex-row gap-2 self-end">
-                    <p className="my-auto">page number</p>
+            </tr>
+          </thead>
+          <tbody className="overflow-scroll">
+            {data?.users.map((user, idx) => (
+              <TableRow
+                index={idx}
+                user={user}
+              />
+            ))}
+            <tr className="bg-gray-50">
+              {data?.problems && (
+                <td
+                  colSpan={2 + data?.problems.length}
+                  className="rounded-b-md py-2">
+                  <div className="flex flex-row gap-2">
+                    <p className="my-auto ml-auto">page number</p>
                     <input
                       value={pageNumber}
                       onChange={(e) => setPageNumber(+e.target.value)}
-                      className="h-8"
+                      className="h-7 w-14 rounded-md text-center"
                     />
                     <p className="my-auto">row per page</p>
                     <input
                       value={rowPerPage}
                       onChange={(e) => setRowPerPage(+e.target.value)}
-                      className="h-8"
+                      className="mr-5 h-7 w-14 rounded-md text-center"
                     />
-                  </td>
-                )}
-              </tr>
-            </tbody>
-          </table>
-        )}
-      </div>
-    </>
+                  </div>
+                </td>
+              )}
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 }
 
--- src/pages/Contest/TableRow.tsx
+++ src/pages/Contest/TableRow.tsx
@@ -9,10 +9,14 @@ export type user = {
 };
 function TableRow({ index, user }: propsType) {
   return (
-    <tr>
-      <th scope="row">{index}</th>
-      <td>{user.username}</td>
-      {user?.scores.map((score) => <td>{score}</td>)}
+    <tr className="bg-gray-50 hover:bg-gray-200">
+      <th
+        scope="row"
+        className="border-b border-gray-400 bg-indigo-200 text-center">
+        {index}
+      </th>
+      <td className="border-b border-l border-gray-400 text-center">{user.username}</td>
+      {user?.scores.map((score) => <td className="border-b border-l border-gray-400 text-center">{score}</td>)}
     </tr>
   );
 }
