fb8dace28ad2ef60827e582ce89964a1d7f9661d
MoJavad Jalilvand <jalilvand.mj@gmail.com>
2024-01-29 21:32:31 +0330

    feat: contest submissions, fix scoreboard

--- src/pages/Contest/TableRow.tsx
+++ src/pages/Contest/TableRow.tsx
@@ -1,12 +1,14 @@
 type propsType = {
   index: number;
-  user: user;
+  user: ScoreboardUserData;
 };
-export type user = {
+
+export type ScoreboardUserData = {
   user_id: number;
-  username: string;
+  user_name: string;
   scores: number[];
 };
+
 function TableRow({ index, user }: propsType) {
   return (
     <tr className="bg-gray-50 hover:bg-gray-200">
@@ -15,7 +17,7 @@ function TableRow({ index, user }: propsType) {
         className="border-b border-gray-400 bg-indigo-200 text-center">
         {index}
       </th>
-      <td className="border-b border-l border-gray-400 text-center">{user.username}</td>
+      <td className="border-b border-l border-gray-400 text-center">{user.user_name}</td>
       {user?.scores.map((score) => <td className="border-b border-l border-gray-400 text-center">{score}</td>)}
     </tr>
   );
